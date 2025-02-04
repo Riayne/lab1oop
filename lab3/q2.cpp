5366764240a3e7c199acd6bd51f302c100671c21
Rehma Fatmi <rehmafatmi@gmail.com>
2025-02-04 09:50:30 +0500

    Update q2.cpp

--- lab3/q2.cpp
+++ lab3/q2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Date{
+    private:
+        int month;
+        int day;
+        int year;
+        
+    public:
+    	int displayDate(){
+		cout<<"Enter day month and year: ";
+		cin>>day>>month>>year;
+		cout<<month<<"/"<<day<<"/"<<year;
+		}
+        
+};
+
+int main(){
+    Date obj;
+    obj.displayDate();
+
+    return 0;
+}
 
